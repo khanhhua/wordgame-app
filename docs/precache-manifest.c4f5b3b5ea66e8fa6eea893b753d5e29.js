6532bb8e82e35a435e47420e8d3a0735c943462d
Khanh Hua <kh@maehren.digital>
2020-03-09 00:12:27 +0100

    Added docs

---
+++ docs/precache-manifest.c4f5b3b5ea66e8fa6eea893b753d5e29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851dba86b5e395155d4efc0ef2dd7bbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d48a4671a3fef7b57a0b",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e6e60f32878672511730",
+    "url": "/static/css/main.464b8e12.chunk.css"
+  },
+  {
+    "revision": "d48a4671a3fef7b57a0b",
+    "url": "/static/js/2.c81dd5eb.chunk.js"
+  },
+  {
+    "revision": "4c8aad0d666452061d9caff27bcc2f63",
+    "url": "/static/js/2.c81dd5eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e60f32878672511730",
+    "url": "/static/js/main.2d94a7d5.chunk.js"
+  },
+  {
+    "revision": "d1c97bcd1eda305f1f33",
+    "url": "/static/js/runtime-main.7fc8a293.js"
+  }
+]);
