b0113c81a2f6bc9064b5e182d6365775d0aa4fce
Khanh Hua <kh@maehren.digital>
2020-03-11 22:32:35 +0100

    Color code the worst performers badges

--- src/pages/ReportPage.js
+++ src/pages/ReportPage.js
@@ -5,6 +5,20 @@ import LineChartWeeklyPerformance from '../components/LineChartWeeklyPerformance
 import BarChartHistogram from '../components/BarChartHistogram';
 import Loader from '../components/Loader';
 
+const bgClasses = {
+  MAS: 'bg-masculine',
+  FEM: 'bg-feminine',
+  NEU: 'bg-neuter',
+};
+
+const classFromTags = (tags) => {
+  const match = /MAS|FEM|NEU/.exec(tags);
+  if (match) {
+    return bgClasses[match[0]];
+  }
+  return '';
+};
+
 export default (props) => {
   const [status, setStatus] = useState({ busy: false, error: null });
   const [report, setReport] = useState({
@@ -46,7 +60,7 @@ export default (props) => {
             <CardBody>
               {report.worstPerformers.map(item => (
               <Badge
-                pill color="warning" className="p-2 mr-2 mb-2"
+                pill className={`p-2 mr-2 mb-2 ${classFromTags(item.tags)}`}
                 style={{ opacity: (1.05 - item.confidence_factor) }}
               >
                 {item.word}
