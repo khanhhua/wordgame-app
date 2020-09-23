6fc4cd46a095dd257ed9f9af045bbf555ed7af6c
khanhhua <khanhhua@users.noreply.github.com>
2020-09-23 19:41:09 +0000

    deploy: b4f46c6ca342a7d8fc1f286bd8592bf1729a9b7d

---
+++ precache-manifest.9fa5b88e3bbd9fda580f0a6b3cfc07be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f1f6956d1c3ab5d771cf44836c18e6",
+    "url": "http://hallodeutsch.fatmandesigner.com/index.html"
+  },
+  {
+    "revision": "82b37f6d64fa93793826",
+    "url": "http://hallodeutsch.fatmandesigner.com/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "54927e5f98f16528dcab",
+    "url": "http://hallodeutsch.fatmandesigner.com/static/css/main.190bcdc3.chunk.css"
+  },
+  {
+    "revision": "82b37f6d64fa93793826",
+    "url": "http://hallodeutsch.fatmandesigner.com/static/js/2.67ed5a2d.chunk.js"
+  },
+  {
+    "revision": "4c8aad0d666452061d9caff27bcc2f63",
+    "url": "http://hallodeutsch.fatmandesigner.com/static/js/2.67ed5a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54927e5f98f16528dcab",
+    "url": "http://hallodeutsch.fatmandesigner.com/static/js/main.c806d727.chunk.js"
+  },
+  {
+    "revision": "4290d44d066ade5888ff",
+    "url": "http://hallodeutsch.fatmandesigner.com/static/js/runtime-main.beb9dcd5.js"
+  }
+]);
