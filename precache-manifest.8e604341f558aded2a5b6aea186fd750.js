76dadf2e9bfc718d15b409819110b2a92ba7ead0
khanhhua <khanhhua@users.noreply.github.com>
2020-09-23 17:06:21 +0000

    deploy: fa815c9d053640112af886eba578b7675b4df992

---
+++ precache-manifest.8e604341f558aded2a5b6aea186fd750.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ceea66274617718fca5447fb4df2ead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3e2e92b24d0ffa7c60f",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "81a1c3102fc0894e22da",
+    "url": "/static/css/main.190bcdc3.chunk.css"
+  },
+  {
+    "revision": "b3e2e92b24d0ffa7c60f",
+    "url": "/static/js/2.ad5f3f63.chunk.js"
+  },
+  {
+    "revision": "4c8aad0d666452061d9caff27bcc2f63",
+    "url": "/static/js/2.ad5f3f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a1c3102fc0894e22da",
+    "url": "/static/js/main.95d55287.chunk.js"
+  },
+  {
+    "revision": "d1c97bcd1eda305f1f33",
+    "url": "/static/js/runtime-main.7fc8a293.js"
+  }
+]);
