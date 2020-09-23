00edd46127c4d1201110716e5135af71442a7fbd
khanhhua <khanhhua@users.noreply.github.com>
2020-09-23 17:17:05 +0000

    deploy: c9adac080328ea6cc3e0b92f9e2d3d4fda19ceb3

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e604341f558aded2a5b6aea186fd750.js"
+  "https://www.khanhhua.com/wordgame-app/public/precache-manifest.0bab5f1388e6259a51aa0b389a9987c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://www.khanhhua.com/wordgame-app/public/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
